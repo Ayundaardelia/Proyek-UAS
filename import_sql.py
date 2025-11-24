58a18a8e90203fbcc776380a9f14134fa6d1d368
Ayundaardelia <ayundaardelia@gmail.com>
2025-11-24 22:48:54 +0700

    coba database

--- import_sql.py
+++ import_sql.py
@@ -2,11 +2,19 @@ import pandas as pd
 from sqlalchemy import text
 from database import engine
 
-# Query ke MySQL
+# Query dari SQL
 query = text("SELECT * FROM waste_management_and_recycling_india")
 
-# Ambil data ke DataFrame
+# Load ke DataFrame
 df = pd.read_sql(query, engine)
 
-# Print ke terminal VSCode
-print(df)
+# Export ke CSV
+df.to_csv("waste_data_export.csv", index=False)
+
+print("✅ Data berhasil diexport ke waste_data_export.csv")
+
+print("======= 5 DATA PERTAMA =======")
+print(df.head(5).to_string())
+
+print("\n======= 5 DATA TERAKHIR =======")
+print(df.tail(5).to_string())python import_sql.py
